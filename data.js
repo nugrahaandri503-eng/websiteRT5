ae4e898b221d9ce9df8c79fde2d377081cc8f98a
Andri Bagus Nugraha <nugrahaandri503@gmail.com>
2026-07-12 05:09:51 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
 const dataKT = {
   "organisasi": {
-    "nama": "Trah Yudistira",
-    "slogan": "Aditya Karya Mahatva Yodha",
+    "nama": "REMAJA KARANG TARUNA",
+    "slogan": "RT 5 RW 5",
     "keterangan": "Karang Taruna RT 05 RW 05 Bandarharjo"
   },
   "pesan_solidaritas": {
     "teks": "\"NEK ORA SRAWUNG RASAH TULANG TULUNG!!! Nek alesan sesok kerjo, kesel, kuliah, ono tugas... La mangsamu kene seng srawung ki pengangguran po pie!!!\""
   },
-  "tentang": "Trah Yudistira bukan sekadar organisasi, melainkan wadah kebersamaan dan kekeluargaan pemuda-pemudi RT 05 RW 05 Bandarharjo. Kami mencetak pemuda yang cerdas, berkarya, dan berjiwa ksatria untuk lingkungan sekitar.",
+ "tentang": "<b>Trah Yudistira</b> bermakna 'Keluarga yang Bijaksana'. Kami menyadari bahwa Karang Taruna bukan hanya tempat berkumpul, melainkan <b>wadah persaudaraan sejati</b> bagi pemuda-pemudi RT 05 RW 05 Bandarharjo.<br><br>Napas pergerakan kami berpedoman pada semboyan <b>Aditya Karya Mahatva Yodha</b>, yang bermakna pemuda yang cerdas (Aditya), aktif berkarya (Karya), berbudi luhur (Mahatva), dan berjiwa ksatria (Yodha). Dengan merangkul semua golongan, kami hadir untuk <b>memecahkan masalah bersama</b> dengan kepala dingin, menghidupkan kembali tradisi srawung, dan menciptakan lingkungan yang <b>rukun, aktif, serta progresif</b>.",
   "arah_gerak": {
     "visi": "Mewujudkan generasi muda yang mandiri, inovatif, berakhlak mulia, serta memiliki solidaritas tinggi dalam membangun lingkungan yang harmonis.",
     "misi": [
@@ -18,7 +18,7 @@ const dataKT = {
     ]
   },
   "kontak": {
-    "link_form": "https://docs.google.com/forms/d/19eWJh8J6Mx9DrGXKEv3ojKmqw8Cv9pscK_eCAIzU0bxt1wlaTw/viewform?hl=id",
+    "link_form": "https://docs.google.com/forms/d/e/1FAIpQLSd8SVZZ003DLpQUQXUzQSB8o8LduGQXIgKCFDlVbVTtCcPHoA/viewform?usp=dialog",
     "tahun_copyright": "2026"
   }
 };
